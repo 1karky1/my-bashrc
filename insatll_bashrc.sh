ddd06df97741c022dafc605ff5a0604305fd0824
karky <1karky1@gmail.com>
2019-07-10 19:24:52 +0200

    small update

--- insatll_bashrc.sh
+++ insatll_bashrc.sh
@@ -2,8 +2,9 @@
 COMMAND="[[ -f ~/.mybashrc ]] && . ~/.mybashrc"
 BASHRC_PATH="$HOME/.bashrc"
 MY_BASHRC_PATH="$HOME/.mybashrc"
+REPO_BASHRC_PATH=".bashrc"
 
-[[ -f ${MY_BASHRC_PATH} ]]  || cp ".bashrc" ${MY_BASHRC_PATH}
-cmp --silent $old $new || cp ".bashrc" ${MY_BASHRC_PATH}
+[[ -f ${MY_BASHRC_PATH} ]]  || cp ${REPO_BASHRC_PATH} ${MY_BASHRC_PATH}
+cmp --silent ${MY_BASHRC_PATH} ${REPO_BASHRC_PATH} || cp ${REPO_BASHRC_PATH} ${MY_BASHRC_PATH}
 grep -qxF "${COMMAND}" ${BASHRC_PATH} || echo "${COMMAND}" >> ${BASHRC_PATH}
-source ${BASHRC_PATH}
+exec bash
